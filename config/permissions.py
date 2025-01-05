ed269f361b84341b7d387bfbda730690505aa276
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2025-01-06 02:02:25 +0330

    feat: create superuser permission

--- config/permissions.py
+++ config/permissions.py
@@ -1,9 +1,19 @@
 from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
 
 
 class IsSuperUser(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_superuser
+
+
+class IsSuperUserOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS or
+            request.user and
+            request.user.is_superuser
+        )
 
 
 class IsCurrentUser(permissions.BasePermission):
