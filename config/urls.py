528a15bc637e0dfc93a287509ee7a1d6ba6d4ae7
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2025-01-06 02:02:09 +0330

    feat add warehouse and swagger url

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('api/', include('users.urls')),
-                  path('api/', include('informing.urls')),
+    path('admin/', admin.site.urls),
+    path('api/', include('users.urls')),
+    path('api/', include('informing.urls')),
+    path('api/', include('warehouse.urls')),
 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+        path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+        path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    ]
